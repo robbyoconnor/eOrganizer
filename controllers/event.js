3c12ca72310dbbba63973ad0503d58fda0eb2e0c
apedneka <pednekar.ameya@gmail.com>
2016-03-11 17:32:42 -0500

    form working and posts storing to db

--- controllers/event.js
+++ controllers/event.js
@@ -1,3 +1,20 @@
-exports.getEvent = function(req, res) {
-	res.render('event');
+var _ = require('lodash');
+var Event = require('../models/Event');
+var secrets = require('../config/secrets');
+
+exports.postEvent = function(req, res, next) {
+	
+	var event = new Event({
+    name: req.body.title,
+    description: req.body.desc
+  });
+
+	event.save(function (err) {
+  	if (err) return next(err);
+	res.redirect('/');
+	});
+};
+
+exports.getEvent = function(req,res,next) {
+	res.render('event')
 };
--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,7 @@
  * GET /
  * Home page.
  */
+//var secrets = require('../config/secrets');
 exports.index = function(req, res) {
   res.render('home', {
     title: 'Home'
